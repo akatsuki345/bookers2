491f48222244199d39a73358a5d395e1851ad50f
akatsuki345 <86pe5ice@gmail.com>
2023-06-27 09:02:52 +0000

    途中 bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'books/new'
-  get 'books/create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
-  get 'home/top'
-  get 'home/about'
+  resources :books
+  resources :users
+  get 'home/top' => 'home#top'
+  get 'home/about' => "home#about", as: "about"
   root to: "home#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
